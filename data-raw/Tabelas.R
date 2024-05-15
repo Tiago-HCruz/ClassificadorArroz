c72dd2385bbb8c6b26880f6f88d127a1a7dd01f8
Tiago-HCruz <tihendacruz@gmail.com>
2024-05-15 15:42:27 -0300

    Add tabelas dos dados LDA/QDA

--- data-raw/Tabelas.R
+++ data-raw/Tabelas.R
@@ -7,11 +7,15 @@ Acuracia <- tibble(LDA = Acuracia_LDA,
                    QDA = Acuracia_QDA)
 
 #usethis::use_data(Acuracia, overwrite = TRUE)
+tab_LDA <- tab1
+tab_QDA <- tab2
+usethis::use_data(tab_LDA, overwrite = TRUE)
+usethis::use_data(tab_QDA, overwrite = TRUE)
 
-tab1 |>
-  as.data.frame.matrix() |>
-  as_tibble()
+tab1 #|>
+  #as.data.frame.matrix() |>
+  #as_tibble()
 
-tab2 |>
-  as.data.frame.matrix() |>
-  as_tibble()
+tab2 #|>
+  #as.data.frame.matrix() |>
+  #as_tibble()
