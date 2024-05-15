ee1c81e894f1c2b3d001492839949abf25a2765a
Tiago-HCruz <tihendacruz@gmail.com>
2024-05-15 16:13:45 -0300

    Adicionando Matriz de Confusão das LDA e QDA

--- data-raw/Tabelas.R
+++ data-raw/Tabelas.R
@@ -7,15 +7,17 @@ Acuracia <- tibble(LDA = Acuracia_LDA,
                    QDA = Acuracia_QDA)
 
 #usethis::use_data(Acuracia, overwrite = TRUE)
+
+
+tab1 <- tab1 |>
+  as.data.frame.matrix() |>
+  as_tibble()
+
+tab2 <- tab2 |>
+  as.data.frame.matrix() |>
+  as_tibble()
+
 tab_LDA <- tab1
 tab_QDA <- tab2
 usethis::use_data(tab_LDA, overwrite = TRUE)
 usethis::use_data(tab_QDA, overwrite = TRUE)
-
-tab1 #|>
-  #as.data.frame.matrix() |>
-  #as_tibble()
-
-tab2 #|>
-  #as.data.frame.matrix() |>
-  #as_tibble()
