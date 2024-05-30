9b43faf83eaf9bad37681821d200aea01a512722
Tiago Cruz <140270380+Tiago-HCruz@users.noreply.github.com>
2024-05-30 12:28:39 -0300

    Delete data-raw/Grafico_de_barras_DB.R

--- data-raw/Grafico_de_barras_DB.R
+++
@@ -1,5 +0,0 @@
-Quant_Rice |>
-  pivot_longer(!Classe, names_to = "Dados", values_to = "Quantidade") |>
-  ggplot(aes(x = Dados, y = Quantidade, fill = Classe))+
-  geom_col(position = "dodge") +
-  labs(title = "Grafico de barras entre os Banco de dados")
